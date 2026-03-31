5ef6c59f397e6bf58ea55cdb7994d84dfbf0215d
root <root@localhost.localdomain>
2026-03-31 13:45:52 +0800

    fix: production build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['hot-walls-visit.loca.lt', 'loca.lt'],
 };
 
 export default nextConfig;
